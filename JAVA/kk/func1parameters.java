3060daf6a7807cc821d96325389d3f198cbce65f
vanshika-verma14 <vanshika9994verma@gmail.com>
2024-12-17 23:45:17 +0530

    dsa

--- JAVA/kk/func1parameters.java
+++ JAVA/kk/func1parameters.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class funcparameters {
+public class func1parameters {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your name: ");
