6538f1f67312d003f420bc26f629c9b62a6b142c
vanshika-verma14 <vanshika9994verma@gmail.com>
2024-12-27 20:12:35 +0530

    dsa

---
+++ JAVA/kk/Binary Search/BS2_Order_Agnostic.java
@@ -0,0 +1,34 @@
+public class BS2_Order_Agnostic {
+    public static void main(String[] args) {
+        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int target = 22;
+        int ans = binSearch(arr, target);
+        System.out.println(ans);
+    }
+
+    static int  binSearch(int[] arr , int n){
+        int start = 0;
+        int end = arr.length - 1;
+         //ascending order
+        if(arr[start] < arr[end])
+        { while(start <= end) {
+            int mid  = start + (end-start)/2; 
+             if(n < arr[mid])
+             end = mid -1;            
+             else if (n > arr[mid])
+             start = mid +1;
+             
+           }
+        }
+         else 
+         { while(start <= end) {         // decending order{99. 88. 34,33, 27 , 10 , 9}
+            int mid  = start + (end-start)/2; 
+             if(n < arr[mid])
+             start = mid +1;      
+             else if (n > arr[mid])
+             end = mid -1; 
+           }
+           }
+           
+}
+}
