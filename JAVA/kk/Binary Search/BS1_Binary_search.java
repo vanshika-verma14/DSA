812b2469deb9fdb0fea634495686b070d461fea0
vanshika-verma14 <vanshika9994verma@gmail.com>
2024-12-28 14:57:46 +0530

    dsa

--- JAVA/kk/Binary Search/BS1_Binary_search.java
+++ JAVA/kk/Binary Search/BS1_Binary_search.java
@@ -1,6 +1,6 @@
 public class BS1_Binary_search {
     public static void main(String[] args) {
-        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89 };
         int target = 22;
         int ans = binSearch(arr, target);
         System.out.println(ans);
@@ -14,7 +14,7 @@ public class BS1_Binary_search {
 
           if(n < arr[mid])
           end = mid -1;
-           
+
           else if (n > arr[mid])
           start = mid +1;
           
---
+++ JAVA/kk/Binary Search/BS3_Ceiling_of_Number.java
@@ -0,0 +1,6 @@
+public class BS3_Ceiling_of_Number {
+    public static void main(String[] args) {
+        
+    }
+
+}
