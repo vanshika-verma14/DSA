71c3083d0454ffb6f5b2339eba317d881d162b16
vanshika-verma14 <vanshika9994verma@gmail.com>
2024-12-29 23:26:51 +0530

    dsa

--- JAVA/kk/Binary Search/BS5_First_Last_Position_of_Element.java
+++ JAVA/kk/Binary Search/BS5_First_Last_Position_of_Element.java
@@ -1,9 +1,28 @@
 public class BS5_First_Last_Position_of_Element{
     public static void main(String[] args) {
         int[] arr = {5,7,7,8,8,10};
-        int target = 22;
+        int target = 7;
         int ans = check(arr, target);
         System.out.println(ans);
     
     }
+    static int  check(int[] arr , int n){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end) {
+         int mid  = start + (end-start)/2;
+
+          if(n < arr[mid])
+          end = mid -1;
+
+          else if (n > arr[mid])
+          start = mid +1;
+          
+          else 
+          return arr[mid];
+
+        }
+        return -1;
+    }
+
 }
--- JAVA/kk/Binary Search/BS6_SmallestLetter_leet.java
+++ JAVA/kk/Binary Search/BS6_SmallestLetter_leet.java
@@ -1,3 +1,24 @@
 public class BS6_SmallestLetter_leet{
+    public static void main(String[] args) {
+        char[] arr = {'c','f','j' };
+        char target = 'e';
+        // System.out.println("1");
+         System.out.println( binSearch(arr, target));
+    }
+    static char  binSearch(char[] arr , char n){
 
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end) {
+         int mid  = start + (end-start)/2;
+
+          if(n < arr[mid])
+          end = mid -1;
+
+          else
+          start = mid +1;
+          
+        }
+        return arr[start % arr.length];
+    }
 }
