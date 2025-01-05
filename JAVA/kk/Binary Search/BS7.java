8f5730a56252a1b34750bb67d137e6f0fb1bbcc8
vanshika-verma14 <vanshika9994verma@gmail.com>
2025-01-05 23:25:40 +0530

    dsa

--- JAVA/kk/Binary Search/BS7.java
+++ JAVA/kk/Binary Search/BS7.java
@@ -1,3 +1,23 @@
 public class BS7 {
-    
+    public static void main(String[] args) {
+        int[] arr = {0,10,5,2};
+        System.out.println(peakIndexInMountainArray( arr));
+    }
+
+    public static int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+        
+        while(start < end){
+        int mid = start + (end- start)/2;
+          
+          if(arr[mid] > arr[mid+1] )
+          end = mid;
+
+          else 
+            start = mid + 1;
+          
+        }
+        return start;
+    }
 }
