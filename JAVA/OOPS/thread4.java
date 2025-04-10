c31b99043c14ce429e6aa14d70289d56095c4790
vanshika-verma14 <vanshika9994verma@gmail.com>
2025-04-10 23:41:04 +0530

    oopsz

--- JAVA/OOPS/thread4.java
+++ JAVA/OOPS/thread4.java
@@ -1,22 +1,33 @@
 class thrd1 extends Thread{
     public void run(){
-        while (true){
+        int i =0;
+        while (i <1000){
             System.out.println("HELLOOOO");
+            i++;
         }
     }
 }
 class thrd2 extends Thread{
     public void run(){
-        while (true){
-            System.out.println("THANK YOUUU");
+        int i =0;
+        while (i <1000){
+            System.out.println("THANK YOUU");
+            i++;
         }
     }
 }
 public class thread4 {
     public static void main(String[] args) {
         thrd1 t1 = new thrd1();
-        thrd1 t2 = new thrd1();
+        thrd2 t2 = new thrd2();
         t1.start();
+        try{
+            t1.join();
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        
         t2.start();
 
         }
