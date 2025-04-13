a456b32c915ab6ab8785fe1166d7ac4a9c60301e
vanshika-verma14 <vanshika9994verma@gmail.com>
2025-04-13 12:25:13 +0530

    oopsz

---
+++ JAVA/OOPS/kkabstrac.java
@@ -0,0 +1,4 @@
+//if class contain any one abstract methd then class is declared as abstract 
+// you cannot creae object of an abstract class
+// Abstract classes: Classes that cannot be instantiated and are intended to be extended.
+// Abstract methods: Methods that do not have a body and must be implemented by subclasses.
