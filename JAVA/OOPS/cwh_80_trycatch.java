5d0251d09a5a94dca1c22ab9c3265710f95e793b
vanshika-verma14 <vanshika9994verma@gmail.com>
2025-04-14 19:19:45 +0530

    oopsz

---
+++ JAVA/OOPS/cwh_80_trycatch.java
@@ -0,0 +1,19 @@
+public class cwh_80_trycatch {
+   public static void main(String[] args) {
+    int a = 6000;
+    int b = 0;
+    // Without Try:
+    // int c = a / b;
+    // System.out.println("The result is " + c);
+        // With Try:
+    try{
+        int c = a/b;
+    System.out.println("RESULT IS "  + c);
+    }
+    catch (Exception e){
+        System.out.println("We failed to divide. Reason: ");
+        System.out.println(e);
+    }
+    System.out.println("End of the program");
+   } 
+}
