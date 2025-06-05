74d6a655152a902f0525e1cde7aba9275d85a8fd
vanshika-verma14 <vanshika9994verma@gmail.com>
2025-06-06 02:19:37 +0530

    oop

--- JAVA/kk/Sorting Techniques/rev/missingnum.java
+++ JAVA/kk/Sorting Techniques/rev/missingnum.java
@@ -1,27 +1,30 @@
 import java.util.Arrays;
-
-public class cyclic_sort_1 {
-    public static void main(String[] args) {
-        int[] arr = {3,4,5,1,2};
+public class missingnum {
+        public static void main(String[] args) {
+        int[] arr = {3,4,0,1,2};
         System.out.println(Arrays.toString(cyclic(arr)));
-        }
-        
+        }       
         public static int[] cyclic(int[] arr) {
             int i=0; 
+            int n = arr.length;
             while(i < arr.length){
-                int correct =arr[i]-1;
+                int correct =arr[i];
+
 
                 if(arr[i] != arr[correct])
                 {   System.out.println(arr[i] +" " + correct);
                     swap(arr, i, correct);
                     System.out.println(Arrays.toString(cyclic(arr)));
                 }
-                else 
+                else if (arr[i] == n) 
                     i++;
+                else
+                i++;
             }  
+
+            
             return arr;   
     }
-
     public static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
