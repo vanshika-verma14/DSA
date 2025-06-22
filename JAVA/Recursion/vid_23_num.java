3e3bd5c23e4714b7fe7b84fc602ee30999e3ac65
vanshika-verma14 <vanshika9994verma@gmail.com>
2025-06-23 04:47:44 +0530

    dsa

--- JAVA/Recursion/vid_23_num.java
+++ JAVA/Recursion/vid_23_num.java
@@ -1,7 +1,42 @@
 
 public class vid_23_num {
     public static void main(String[] args) {
-        
-        
+        int n = 13545;
+       System.out.println(sum(n));
     }
-}
+    public static void func(int n){
+       if(n==0)
+       return;
+      System.out.println(n);
+
+       func(n-1);
+    }
+     public static void funcrev(int n){
+       if(n==0)
+       return;
+       funcrev(n-1);
+       System.out.println(n);
+    }
+     public static void funcboth(int n){
+       if(n==0)
+       return;
+       System.out.println(n);
+       funcboth(n-1);
+       System.out.println(n);
+    }
+    public static int fact(int n){
+       if(n== 1)
+       return 1;
+       else
+       System.out.println(n);
+       return n * fact(n-1);
+     
+    }
+    
+    public static int sum(int n){
+       if(n== 0)
+       return 0;
+       else
+       return n%10 + sum(n/10);
+    }
+    }
