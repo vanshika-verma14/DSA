6db5de24324cf365bbc1b80ce8cce73ff318a9e4
vanshika-verma14 <vanshika9994verma@gmail.com>
2025-07-21 16:30:39 +0530

    dsa

--- JAVA/Recursion/patterns.java
+++ JAVA/Recursion/patterns.java
@@ -1,5 +1,17 @@
 public class patterns {
     public static void main(String[] args) {
-        
+    pattern(4,0);
+    }
+    public static void pattern(int r, int c){
+        if(r==0)
+        return;
+
+        if(c<r){
+          System.out.print("* ");
+          pattern(r,c+1);}
+        else{
+          System.out.println();
+          pattern(r-1,0);
+        }
     }
 }
