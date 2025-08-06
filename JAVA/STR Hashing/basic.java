b108dd31908a4a154a229dd75f48926bce805f77
vanshika-verma14 <vanshika9994verma@gmail.com>
2025-08-07 02:22:19 +0530

    dsa

---
+++ JAVA/STR Hashing/basic.java
@@ -0,0 +1,24 @@
+public class basic {
+    public static void main(String[] args) {
+        //find frequency of each number without hasg map
+        int[] arr = {2,3,4,5,2,3,6,4,4,4,7,2};
+        freq(arr);
+    }
+
+    public static void freq(int[] arr) {
+
+        int[] fcn = new int[10];
+
+        for (int i = 0; i < arr.length; i++) {
+            fcn[arr[i]] += 1;
+        }
+
+
+        for (int i = 0; i < fcn.length; i++) {
+            if(fcn[i]>0){
+                System.out.println(i+ " = " + fcn[i] );
+            }
+        }
+
+    }
+}
