97a665d6128aa7784a88ca4d36f2eec0339954cc
vanshika-verma14 <vanshika9994verma@gmail.com>
2025-08-22 02:39:57 +0530

    dsa

---
+++ JAVA/Striver/arrays/missing_num.java
@@ -0,0 +1,11 @@
+
+public class missing_num {  //1-N number in original but in given array 1 number is missing
+    public static void main(String[] args) {  
+    int[] arr = {1,2,4,5};
+    System.out.println(check2(arr));
+    }
+
+    public static int check2(int[] arr) {
+      
+    }
+}
---
+++ JAVA/Striver/sort_1_2_3.java
@@ -0,0 +1,21 @@
+public class sort_1_2_3 {
+    public static void main(String[] args) {  
+    int[] arr = {1,1,2,1,2,1,2,0,0,0,2,1};
+    System.out.println(sort(arr));
+    }
+public static int[] sort(int[] arr){
+    int start = 0;
+    int end = arr.length - 1;
+    int mid = start + (start-end)/2;
+    int l = mid ;
+    int r = mid ;
+    for (int i = 0; i < arr.length; i++) {
+        if(l==0 || r==0){
+            swap
+        }
+    }
+
+    return arr;
+
+}
+}
