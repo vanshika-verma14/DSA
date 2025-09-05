8917efb121403c533de1a772d47756a6626ec1b8
vanshika-verma14 <vanshika9994verma@gmail.com>
2025-09-05 05:37:17 +0530

    dsa

--- JAVA/Striver/arrays/Kadens_algo.java
+++ JAVA/Striver/arrays/Kadens_algo.java
@@ -1,14 +1,19 @@
 //find the subarray that gives the max sum
+//53. Maximum Subarray
+// Given an integer array nums, find the subarray with the largest sum, and return its sum.
+// Example 1:
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
 
 import java.util.Arrays;
-
 public class Kadens_algo {
     public static void main(String[] args) {
-        int[] arr = {-2,-3,4,-1,-2,1,5,-2,-7};
-        System.out.println(kadane(arr));
+        int[] arr = {-2,-3,-6,-1};
+        System.out.println(maxSubArray(arr));
 
     }
-    public static int kadane(int[] arr){
+    public static int kadane(int[] arr){ // here we are keeping track of longest subarray as well
         int max= Integer.MIN_VALUE;
         int sum = 0;
         int start = -1;
@@ -18,18 +23,32 @@ public class Kadens_algo {
             if(sum==0)
             start= i; //to print subarray if 0 comes we start the marking of subaaray again
             sum+=arr[i];
-            //System.out.println(sum);
-            if(sum<0){
-                sum=0;
-                start = i;
-            }
             if(sum>max){
                 max=sum;
                 starti = start; endi =i; // keep a track of index at start and end of subarray
             }
+             //System.out.println(sum);
+            if(sum<0){
+                sum=0;
+                start = i;
+            }
 
         }
         System.out.println(starti + " " + endi);
+        return max;
+    }
+
+    //leetcode paste , justthe max sum of the longest subarray
+    public static int maxSubArray(int[] nums) {
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i= 0; i<nums.length; i++){
+            sum+=nums[i];
+            max = Math.max(sum,max);  // check max before setting sum zero so that in case of negative no. only in subarray that max of that no. can be printed else sum will give 0 if done before
+
+            if(sum<0)
+            sum=0;
+        }
         return max;
     }
 }
